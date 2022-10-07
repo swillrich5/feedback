add9aee3d87deb79290f4ade80c956ffdf59fa95
Scott Willrich <swillrich@gmail.com>
2022-10-06 21:03:38 -0500

    modelform and class based views added

--- reviews/models.py
+++ reviews/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class Review(models.Model):
     user_name = models.CharField(max_length=100)
     review_text = models.TextField()
-    rating = models.IntegerField()
+    rating = models.IntegerField() 
--- reviews/views.py
+++ reviews/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
 
 from .forms import ReviewForm
-from .models import Review
 
 # Create your views here.
+
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+    def post(self, request):
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+
+
 def review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
 
         if form.is_valid():
             print(form.cleaned_data)
-            review = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating'])
-            review.save()
+            form.save()
             return HttpResponseRedirect("/thank-you")
     else:
         form = ReviewForm()
