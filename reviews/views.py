b074d7a86b940da7049e133dc629fec420b37e3d
Scott Willrich <swillrich@gmail.com>
2022-10-07 10:04:38 -0500

    adding TemplateView

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 
@@ -39,5 +40,10 @@ def review(request):
     })
 
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['message'] = "Happy trails to you!"
+        return context
