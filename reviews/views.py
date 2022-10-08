4c55de9121e2b51a66a255205e51b86f5cb15112
Scott Willrich <swillrich@gmail.com>
2022-10-07 19:20:06 -0500

    replaced FormView with CreateView for creating Reviews

--- reviews/views.py
+++ reviews/views.py
@@ -3,45 +3,18 @@ from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
-class ReviewView(FormView):
+class ReviewView(CreateView):
+    model = Review
     form_class = ReviewForm
     template_name = "reviews/review.html"
     success_url = "/thank-you"
-
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
-
-    # def post(self, request):
-    #     form = ReviewForm(request.POST)
-
-    #     if form.is_valid():
-    #         print(form.cleaned_data)
-    #         form.save()
-    #         return HttpResponseRedirect("/thank-you")
-
-
-def review(request):
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            print(form.cleaned_data)
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-    else:
-        form = ReviewForm()
-
-    return render(request, "reviews/review.html", {
-       "form": form
-    })
 
 
 class ThankYouView(TemplateView):
