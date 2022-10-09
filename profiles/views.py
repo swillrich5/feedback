3252252ec16d9bd1c0efc23bde7698f1b1ff48db
Scott Willrich <swillrich@gmail.com>
2022-10-09 15:07:17 -0500

    upload working

--- profiles/views.py
+++ profiles/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import ProfileForm
+from .models import UserProfile
 
+# Create your views here.
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        print(request.FILES["image"])
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+            return HttpResponseRedirect("/profiles")
+        
+        return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        })
